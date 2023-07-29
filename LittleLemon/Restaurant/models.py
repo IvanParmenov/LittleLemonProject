f19879fed13608adb5943143c54d8f8a21b92c6d
Ivan Parmenov <ivanparmenov@gmail.com>
2023-07-30 01:05:40 +0400

    message

--- LittleLemon/Restaurant/models.py
+++ LittleLemon/Restaurant/models.py
@@ -14,9 +14,8 @@ class Booking(models.Model):
 
 
 
-    def __str__(self):
-        return f'{self.name}: {self.booking_date}'
-
+    def __str__(self) -> str:
+        return f'{self.name} for {self.number_of_guests} guests on {self.booking_date}'
 
 class Menu(models.Model):
     title = models.CharField(max_length=255)
@@ -27,5 +26,5 @@ class Menu(models.Model):
         verbose_name = 'Menu'
         verbose_name_plural = 'Menu Items'
 
-    def __str__(self):
-        return f'{self.title}: ${self.price:.2f}'
+    def __str__(self) -> str:
+        return f'{self.title} : {str(self.price)}'
